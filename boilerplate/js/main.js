7c0f814e48041a5aa3b93da253dc75f3f553ec86
Kutnayk Evgeniy <ifgen09@gmail.com>
2014-06-28 18:44:13 +0300

    boilerplate upload

---
+++ boilerplate/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	doSmth('one',2,true);
+
+
+});/*end of document ready*/
+
+
+/**
+ *
+ * @param first {string}
+ * @param second {number}
+ * @param third {boolean}
+ * @returns {boolean}
+ */
+function doSmth(first,second,third){
+	var c = false;
+	return c;
+}
